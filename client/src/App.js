ea8d8826fc5c85339215993ffa20e75413f86924
DrMustafaH <mustafaharith@gmail.com>
2021-03-12 10:00:14 -0800

    Implement About us section

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import LandingSection from "./components/Homepage/LandingSection";
+import AboutUs from "./components/Homepage/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingSection />
+      <AboutUs />
     </div>
   );
 }
---
+++ client/src/components/Homepage/AboutUs.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./AboutUs.scss";
+// import Button from "@material-ui/core/Button";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1 className="about-us-heading">About Us</h1>
+      <div className="about-us-section">
+        <div>
+          <img
+            alt="logo"
+            src="https://i.pinimg.com/originals/a5/e0/2a/a5e02a5a141f2a2a7b588edf7b467f45.jpg"
+          />
+          <p>Some text</p>
+        </div>
+        <div>
+          <img
+            alt="logo"
+            src="https://i.pinimg.com/originals/a5/e0/2a/a5e02a5a141f2a2a7b588edf7b467f45.jpg"
+          />
+          <p>Some text</p>
+        </div>
+      </div>
+    </div>
+  );
+}
