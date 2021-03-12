b8ed26b47ff34b7c2008849a5f5bdc373c75f7f6
DrMustafaH <mustafaharith@gmail.com>
2021-03-11 18:30:44 -0800

    Install material UI and start implemting navbar component

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img
+        alt="logo"
+        src="https://i.pinimg.com/originals/a5/e0/2a/a5e02a5a141f2a2a7b588edf7b467f45.jpg"
+      />
+      <ul></ul>
+    </nav>
+  );
+}
