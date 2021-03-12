ccdca713da940f3d65cf8b66b9633d8b7ac1f977
DrMustafaH <mustafaharith@gmail.com>
2021-03-12 11:11:48 -0800

    Implement footer and install font awesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import HomePage from "./components/Homepage/Homepage";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <HomePage />
+      <Footer />
     </div>
   );
 }
