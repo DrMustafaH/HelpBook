6a7205440ac2bc065d63a6cd4e236b3002607bf3
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-03-13 09:37:00 -0800

    Create the Receiver profile component and add header to it

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import HomePage from "./components/Homepage/Homepage";
-import Dashboard from "./components/Dashboard/Dashboard";
-import DonorProfile from "./components/DonorProfile/DonorProfile";
+// import HomePage from "./components/Homepage/Homepage";
+// import Dashboard from "./components/Dashboard/Dashboard";
+// import DonorProfile from "./components/DonorProfile/DonorProfile";
+import ReceiverProfile from "./components/ReceiverProfile/ReceiverProfile";
 
 //homepage
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
+      {/* <HomePage />
       <Dashboard />
-      <DonorProfile />
+      <DonorProfile /> */}
+      <ReceiverProfile />
       <Footer />
     </div>
   );
---
+++ client/src/components/ReceiverProfile/ReceiverProfile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../DonorProfile/Header";
+
+export default function DonorProfile() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
