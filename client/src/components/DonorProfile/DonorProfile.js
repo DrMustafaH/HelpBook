2cde4a59fda87af754e090e6ba6a2649fe36fe22
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-03-12 18:37:43 -0800

    Add subcomponents to render - ActivityFeed, Recommendations...-

--- client/src/components/DonorProfile/DonorProfile.js
+++ client/src/components/DonorProfile/DonorProfile.js
@@ -1,13 +1,21 @@
 import React from "react";
-
+import "./DonorProfile.scss";
 import Header from "./Header";
 import FollowingSection from "./FollowingSection";
+import ActivityFeed from "./ActivityFeed";
+import ScheduledVolunteering from "./ScheduledVolunteering";
+import Recommendations from "./Recommendations";
 
 export default function DonorProfile() {
   return (
     <div>
       <Header />
       <FollowingSection />
+      <div className="two-columns">
+        <ScheduledVolunteering />
+        <ActivityFeed />
+      </div>
+      <Recommendations />
     </div>
   );
 }
