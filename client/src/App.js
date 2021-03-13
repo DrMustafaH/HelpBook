f6d44c201b7aa132090dc9b65e1287a6ac566296
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-03-12 20:52:22 -0800

    Add all components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import HomePage from "./components/Homepage/Homepage";
-// import Dashboard from "./components/Dashboard/Dashboard";
+import HomePage from "./components/Homepage/Homepage";
+import Dashboard from "./components/Dashboard/Dashboard";
 import DonorProfile from "./components/DonorProfile/DonorProfile";
 
 //homepage
@@ -10,21 +10,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <HomePage /> */}
+      <HomePage />
+      <Dashboard />
       <DonorProfile />
       <Footer />
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <Dashboard />
-//       <Footer />
-//     </div>
-//   );
-// }
 
 export default App;
