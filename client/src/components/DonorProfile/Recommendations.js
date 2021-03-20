1dac3cf82a5188bd8fcc0f228f9a061c63435681
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-03-19 17:32:15 -0700

    Updated Recommendation section in donor profile

--- client/src/components/DonorProfile/Recommendations.js
+++ client/src/components/DonorProfile/Recommendations.js
@@ -4,18 +4,13 @@ import { Paper, withStyles } from "@material-ui/core";
 
 const StyledPaper = withStyles({
   root: {
-    height: 300,
     width: "100%",
     color: "#0E212F",
     display: "flex",
     fontFamily: "'Trirong', serif",
     fontWeight: "700",
-    fontSize: "1.3rem",
-    borderRadius: 22,
-    border: "4px solid #f0efec",
     maxHeight: 300,
     overflow: "auto",
-    textDecorationLine: "underline",
   },
 })(Paper);
 
@@ -23,7 +18,7 @@ export default function Header() {
   return (
     <div className="recommendations-section">
       <StyledPaper>
-        <p className="recommendations-heading">News Log</p>
+        <h1 className="recommendations-heading">Recommendations</h1>
       </StyledPaper>
     </div>
   );
