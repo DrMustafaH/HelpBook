a7a6e7055f93ddd70d42cfd7eae44ad94450d312
mafe-rnanda <mf.chavezminjarez@gmail.com>
2021-03-22 21:48:23 -0700

    Update the about us content and layout

--- client/src/components/Homepage/AboutUs.js
+++ client/src/components/Homepage/AboutUs.js
@@ -4,22 +4,34 @@ import "./AboutUs.scss";
 // AboutUs component
 export default function AboutUs() {
   return (
-    <div>
+    <div className="about-us-section">
       <h1 className="about-us-heading">About Us</h1>
-      <div className="about-us-section">
+      <div className="about-us-content">
         <div>
-          <img
-            alt="logo"
-            src="https://i.pinimg.com/originals/a5/e0/2a/a5e02a5a141f2a2a7b588edf7b467f45.jpg"
-          />
-          <p>Some text</p>
+          <div className="team-members">
+            <img alt="logo" src="../images/Mustafa.jpg" className="team-img" />
+            <div className="team-name">Mustafa</div>
+          </div>
+          <p>
+            Just say anything, George, say what ever's natural, the first thing
+            that comes to your mind. Take that you mutated son-of-a-bitch. My
+            pine, why you. You space bastard, you killed a pine. You do? Yeah,
+            it's 8:00. Hey, McFly, I thought I told you never to come in here.
+            Well it's gonna cost you. How much money you got on you?
+          </p>
         </div>
         <div>
-          <img
-            alt="logo"
-            src="https://i.pinimg.com/originals/a5/e0/2a/a5e02a5a141f2a2a7b588edf7b467f45.jpg"
-          />
-          <p>Some text</p>
+          <div className="team-members">
+            <img alt="logo" src="../images/Maria.png" className="team-img" />
+            <div className="team-name">Maria</div>
+          </div>
+          <p>
+            Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin.
+            Halvah croissant candy canes bonbon candy. Apple pie jelly beans
+            topping carrot cake danish tart cake cheesecake. Muffin danish
+            chocolate soufflé pastry icing bonbon oat cake. Powder cake jujubes
+            oat cake. Lemon drops tootsie roll marshmallow halvah carrot cake.
+          </p>
         </div>
       </div>
     </div>
